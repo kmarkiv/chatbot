298d66ac12b0d6ceef6f808273c3cb3637ee230d
Vikram <kmarkiv@gmail.com>
2025-08-13 15:28:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.responses.create(
-             prompt={ "id": "pmpt_688a0932291c81979cdf4f746bd3017906dee59b395d64c8", "version": "5" },
+             prompt={ "id": st.secrets["srhr_prompt_id"], "version": st.secrets["srhr_version"] },
             input=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
